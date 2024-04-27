e7aaef0b124dc86c266720ba5964e86a546aded0
윤형석 <104922729+pledge24@users.noreply.github.com>
2024-04-27 17:10:52 +0900

    Update apikey.js

--- Src/apikey.js
+++ Src/apikey.js
@@ -2,7 +2,7 @@ const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTczM2JlZWJiMmJhNmZlOWRiY2QxYWZiYWMzNGY4NyIsInN1YiI6IjY2MmExNzQwMzNhNTMzMDA5YmQxZDI5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iy6fJyWI7Os_VFgPwtAcFHOby3PIXzVRbiV3cOSk3cc'
+      Authorization: ${{secrets.API_KEY}}
     }
   };
 
