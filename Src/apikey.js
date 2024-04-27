b37a720457173ff704f8507500061603dfd08224
pledge24 <104922729+pledge24@users.noreply.github.com>
2024-04-27 16:11:28 +0900

    apikey 경로 확인 테스트

---
+++ Src/apikey.js
@@ -0,0 +1,9 @@
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTczM2JlZWJiMmJhNmZlOWRiY2QxYWZiYWMzNGY4NyIsInN1YiI6IjY2MmExNzQwMzNhNTMzMDA5YmQxZDI5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iy6fJyWI7Os_VFgPwtAcFHOby3PIXzVRbiV3cOSk3cc'
+    }
+  };
+
+export default options;
