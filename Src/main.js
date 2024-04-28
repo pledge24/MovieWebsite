55a3d8134da9b8be01badbeaf84f1bb7146cf339
pledge24 <104922729+pledge24@users.noreply.github.com>
2024-04-28 17:44:30 +0900

    업데이트 0.02v
    업데이트 내용
    
    기능 추가
    - 마우스를 카드 위에 올리면 카드가 회전합니다.
    - 페이지 하단에 추가된 버튼으로 해당 페이지의 영화 목록을 가져옵니다.
    
    버그 및 수정사항
    - 폰트 추가
    - 영화 데이터를 미국에서 한국 기준으로 변경
    - 일부 변수 이름 변경
    - 때때로 영화 카드를 불러오지 않는 버그 수정

--- Src/main.js
+++ Src/main.js
@@ -3,81 +3,173 @@ import options from "./apikey.js";
 const $cardList = document.querySelector(".card-list");
 const $searchButton = document.querySelector("#search-btn");
 const $searchInputText = document.querySelector("#search-input");
+const $prevButton = document.querySelector("#prev-btn");
+const $nextButton = document.querySelector("#next-btn");
+const $bottomButtonList = document.querySelectorAll(".bottom-elem");
 
-let movie_list;
-let details;
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTczM2JlZWJiMmJhNmZlOWRiY2QxYWZiYWMzNGY4NyIsInN1YiI6IjY2MmExNzQwMzNhNTMzMDA5YmQxZDI5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iy6fJyWI7Os_VFgPwtAcFHOby3PIXzVRbiV3cOSk3cc'
-//   }
-// };
+let movieList;
+let movieConfig;
+let pageNo = 1;
 
-fetch('https://api.themoviedb.org/3/configuration', options)
-  .then(response => response.json())
-  .then(response => {
-    details = response.images;
-    //console.log(response);
+let init = false;
+
+// 웹페이지 처음 진입 시 실행
+if (!init) {
+
+  // base_url가져옴
+  fetch('https://api.themoviedb.org/3/configuration', options)
+    .then(response => response.json())
+    .then(response => {
+      movieConfig = response.images;
+      //console.log(response);
+
+      // 영화 정보 가져옴
+      fetch(`https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=${pageNo}`, options)
+        .then(response => response.json())
+        .then(response => {
+          movieList = response;
+          createMovieCards();
+        })
+        .catch(err => console.error(err));
+    })
+    .catch(err => console.error(err));
+
+  // 페이지 버튼에 이벤트 추가
+  $bottomButtonList.forEach(btn => {
+    btn.addEventListener("click", function () {
+      console.log(btn.textContent);
+      
+      let _nextPageNO = Number(btn.textContent);
+      setPages(_nextPageNO);
+    })
   })
-  .catch(err => console.error(err));
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(response => {
-    movie_list = response; 
-    createMovieCards();
+  init = true;
+}
+
+// 해당 페이지의 영화 목록을 카드로 가져오는 함수
+// nextPageNO: 이동할 페이지 번호
+function setPages(nextPageNO) {
+  // 기존에 있던 카드들 전부 삭제
+  while ($cardList.hasChildNodes()) {
+    $cardList.removeChild($cardList.firstChild);
+  }
+
+  // 페이지 버튼 번호 재설정
+  let _newPageNO = nextPageNO - 2 > 1 ? nextPageNO - 2 : 1;
+  $bottomButtonList.forEach(btn => {
+    btn.textContent = _newPageNO;
+    _newPageNO++;
   })
-  .catch(err => console.error(err));
+  pageNo = _nextPageNO;
+
+  // 다음 페이지 영화카드 추가
+  fetch(`https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=${pageNo}`, options)
+    .then(response => response.json())
+    .then(response => {
+      movieList = response;
+      createMovieCards();
+    })
+    .catch(err => console.error(err));
+  
+}
+
+// 이전 페이지 버튼 클릭 이벤트 추가
+$prevButton.addEventListener("click", function () {
+  if (pageNo <= 1) {
+    alert("맨 처음 페이지입니다.");
+    return;
+  }
+
+  let _nextPageNO = pageNo - $bottomButtonList.length >= 1 ? pageNo - $bottomButtonList.length : 1;
+
+  setPages(_nextPageNO);
+
+})
+
+// 다음 페이지 버튼 클릭 이벤트 추가
+$nextButton.addEventListener("click", function () {
+
+  let _nextPageNO = pageNo + 5;
+  setPages(_nextPageNO);
+})
 
 // 웹페이지 시작 시 카드 생성
-function createMovieCards(){
-  let temp_html;
-  movie_list.results.forEach(movie => {
+function createMovieCards() {
+  let temp_card;
+
+  movieList.results.forEach(movie => {
 
     const _cardContainer = document.createElement('div');
-    _cardContainer.setAttribute("class", "movie-card");
-    _cardContainer.setAttribute("id", `${movie.id}`);
-    _cardContainer.addEventListener('click', function () {
-      alert("영화 id: " + this.id);
-    });
+    _cardContainer.setAttribute("class", "card-container");
 
-    let Full_url = details.base_url + details.poster_sizes[4] + movie.poster_path;
+    let _fullUrl = movieConfig.base_url + movieConfig.poster_sizes[4] + movie.poster_path;
 
-    temp_html = `
-        <img src=${Full_url} alt=${movie.original_title}>
+    temp_card = `
+      <div class="movie-card" id=${movie.id}>
+        <img src=${_fullUrl} alt=${movie.original_title}>
         <h3 class="movie-title">${movie.original_title}</h3>
         <p>${movie.overview}</p>
-        <p>Rating: ${movie.vote_average}</p>
+        <br>
+        <p>평점: ${Number(movie.vote_average).toFixed(1)}</p>
+      </div>
     `;
 
-    _cardContainer.innerHTML = temp_html;
+    _cardContainer.innerHTML = temp_card;
 
+    const _card = _cardContainer.querySelector('.movie-card');
+
+    _cardContainer.addEventListener('click', function () {
+      alert("영화 id: " + _card.id);
+    });
+
+    // 마우스가 해당 범위에서 움직였을 때 발생하는 이벤트
+    _cardContainer.addEventListener('mousemove', function (e) {
+      let x = e.offsetX;
+      let y = e.offsetY;
+      
+      let rotateY = (-150 + x) * 2 / 15;
+     
+      _card.style = `transform : perspective(500px) rotateY(${rotateY}deg) `;
+   
+    })
+
+    // 마우스가 해당 범위를 벗어났을 때 발생하는 이벤트
+    _cardContainer.addEventListener('mouseleave', function (e) {
+
+      _card.style = `
+        transition: transform 1s ease;
+        transform: rotateY(0deg);
+      `;
+
+    })
+
+    // 해당 카드를 카드 리스트에 추가
     $cardList.appendChild(_cardContainer);
 
   });
 }
 
 // 영화 검색 함수
-function searchMovies(){
-  let cards = $cardList.querySelectorAll('.movie-card')
+function searchMovies() {
+  let _cards = $cardList.querySelectorAll('.movie-card')
 
   const search_text = $searchInputText.value;
-  cards.forEach(movie_card =>{
+  _cards.forEach(movie_card => {
     let movie_name = movie_card.querySelector('.movie-title').textContent;
-    
+
     // 전부 소문자로 변경.
     let movie_name_lower = movie_name.toLowerCase();
     let search_text_lower = search_text.toLowerCase();
 
     // 검색한 문자열이 있으면 해당 카드를 제외하고 display 전부 끈다. 
-    if(movie_name_lower.search(search_text_lower) !== -1){
+    if (movie_name_lower.search(search_text_lower) !== -1) {
 
       console.log(`I found this movie: ${movie_name}`);
-      movie_card.style.display = 'block';
+      movie_card.parentElement.style.display = 'block';
     }
-    else{
-      movie_card.style.display = 'none';
+    else {
+      movie_card.parentElement.style.display = 'none';
     }
   });
 
@@ -87,22 +179,23 @@ function searchMovies(){
 
 
 // 검색 버튼 클릭 시 검색 함수 호출
-$searchButton.addEventListener("click", function (e){
+$searchButton.addEventListener("click", function (e) {
   e.preventDefault();
   console.log("버튼 이벤트");
   searchMovies();
 });
 
 // 엔터 시 검색 함수 호출
-$searchInputText.addEventListener("keyup", function (e){ 
+$searchInputText.addEventListener("keyup", function (e) {
   // keycode가 13(엔터키 코드)이면 검색 함수 호출
-  if(e.keyCode === 13){
+  if (e.keyCode === 13) {
     e.preventDefault();
     console.log("엔터");
     searchMovies();
   }
-  else{
+  else {
     console.log("다른거");
+    searchMovies();
   }
 });
 
